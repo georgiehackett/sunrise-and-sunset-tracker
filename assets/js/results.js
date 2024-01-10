5a334546aa1f9509f4015f36fc4567ba92e2941c
Georgie <georgiehackett47@gmail.com>
2024-01-10 17:23:47 +0000

    Add new js file for results page

---
+++ assets/js/results.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  // Build URL to query sunrisesunset API
+  var lat = "";
+  var lng = "";
+  var sunriseSunsetURL =
+    "https://api.sunrisesunset.io/json?lat=38.907192&lng=-77.036873";
+
+  // Create fetch call for the search query
+  fetch(sunriseSunsetURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+
+      // Assign data from the object to variables
+      var sunrise = data.results.sunrise;
+      console.log("Sunrise: " + sunrise);
+
+      var sunset = data.results.sunset;
+      console.log("Sunset: " + sunset);
+
+      var goldenHour = data.results.golden_hour;
+      console.log("Golden Hour: " + goldenHour);
+
+      var timezone = data.results.timezone;
+      console.log("Timezone: " + timezone);
+    });
+
+})
